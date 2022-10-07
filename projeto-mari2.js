d0c092b9fee081ca2febbb52b25f3edb1cffc296
Mariana Soares <marianaguti.soares@gmail.com>
2022-10-07 20:43:39 -0300

    week3

---
+++ projeto-mari2.js
@@ -0,0 +1,25 @@
+
+    tenista1 = {
+     nome : "Maria Esther Bueno",
+     grandSlams1 : 7,
+     alive1 : "false",
+     gsWON1 : [ "Wimbledon", 3, "US Open", 4]}
+    
+
+    tenista2 = {
+    nome : "Serena Williams",
+    grandSlams2 : 23,
+    alive2 : "true",
+    gsWON2 : [ "Aus Open", 7, "Roland Garros", 3, "Wimbledon", 7, "US Open", 6]}
+
+
+    tenista3 = { 
+    nome : "Martina Navratilova",
+    grandSlams3 : 18,
+    alive3 : "true",
+    gsWON3 : [ "Aus Open", 3, "Roland Garros", 2, "Wimbledon", 9, "US Open", 4]}
+
+    array = []
+    array.push(tenista1, tenista2, tenista3)
+
+    console.log (array)
